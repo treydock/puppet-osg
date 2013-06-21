23c7b1b076b0b7bc534444144dee9379781b4771
Trey Dockendorf <treydock@gmail.com>
2013-06-20 22:16:18 -0500

    - Cleaner conditional require of facter's file_read or included depending if facter's is available

--- lib/facter/osg_version.rb
+++ lib/facter/osg_version.rb
@@ -3,11 +3,7 @@
 begin
   require 'facter/util/file_read'
 rescue LoadError => e
-  # puppet apply does not add module lib directories to the $LOAD_PATH (See
-  # #4248). It should (in the future) but for the time being we need to be
-  # defensive which is what this rescue block is doing.
-  rb_file = File.join(File.dirname(__FILE__), 'util', 'file_read.rb')
-  load rb_file if File.exists?(rb_file) or raise e
+  require 'facter/util/file_read_ext'
 end
 
 Facter.add(:osg_version) do
