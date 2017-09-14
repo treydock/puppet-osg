da66020b2c82cf0e37940a06c22c612264f77b5f
Trey Dockendorf <tdockendorf@osc.edu>
2017-09-14 15:46:37 -0400

    Allow order to be defined for osg::lcmaps_voms::user and osg::lcmaps_voms::vo

--- manifests/lcmaps_voms/user.pp
+++ manifests/lcmaps_voms/user.pp
@@ -2,12 +2,13 @@
 define osg::lcmaps_voms::user (
   Variant[Array, String] $dn,
   String $user = $name,
+  Integer $order = 50,
 ) {
 
   concat::fragment { "osg::lcmaps_voms::user-${name}":
     target  => '/etc/grid-security/grid-mapfile',
     content => template('osg/lcmaps_voms/mapfile.erb'),
-    order   => '50',
+    order   => $order,
   }
 
 }
