236d20707ae74b7500ba84beb1e55ab21d1ca809
Trey Dockendorf <tdockendorf@osc.edu>
2017-09-25 09:29:04 -0400

    Attempt to fix acceptance tests

--- spec/acceptance/osg_ce_spec.rb
+++ spec/acceptance/osg_ce_spec.rb
@@ -11,11 +11,23 @@ describe 'osg::ce class:' do
         }
         class { 'osg::cacerts::updater': }
         class { 'osg::ce':
-          manage_firewall => false,
-          hostcert_source => 'file:///tmp/hostcert.pem',
-          hostkey_source  => 'file:///tmp/hostkey.pem',
-          httpcert_source => 'file:///tmp/httpcert.pem',
-          httpkey_source  => 'file:///tmp/httpkey.pem',
+          manage_firewall     => false,
+          hostcert_source     => 'file:///tmp/hostcert.pem',
+          hostkey_source      => 'file:///tmp/hostkey.pem',
+          httpcert_source     => 'file:///tmp/httpcert.pem',
+          httpkey_source      => 'file:///tmp/httpkey.pem',
+          site_info_sponsor   => 'foo',
+          site_info_contact   => 'Foo Bar',
+          site_info_email     => 'foo@example.com',
+          site_info_city      => 'Anywhere',
+          site_info_country   => 'USA',
+          site_info_longitude => '0',
+          site_info_latitude  => '0',
+          osg_gip_configs     => {
+            'Subcluster TEST/name'           => { 'value' => 'TEST' },
+            'Subcluster TEST/ram_mb'         => { 'value' => 1024 },
+            'Subcluster TEST/cores_per_node' => { 'value' => 1 },
+          }
         }
       EOS
 
