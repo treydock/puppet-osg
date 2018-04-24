ab596d274ed1497e0d6c12945ff7f0d9938de391
Trey Dockendorf <tdockendorf@osc.edu>
2018-04-24 13:47:33 -0400

    Fix unit tests

--- spec/unit/facter/osg_version_spec.rb
+++ spec/unit/facter/osg_version_spec.rb
@@ -15,7 +15,7 @@ describe 'osg_version fact' do
 
   it "should return correct version 3.2.30" do
     allow(File).to receive(:exists?).with('/etc/osg-version').and_return(true)
-    Facter::Core::Execution.expects(:execute).with('cat /etc/osg-version 2>/dev/null').returns(my_fixture_read('osg-version-3.2.30'))
+    allow(Facter::Core::Execution).to receive(:execute).with('cat /etc/osg-version 2>/dev/null').and_return(my_fixture_read('osg-version-3.2.30'))
     expect(Facter.fact(:osg_version).value).to eq('3.2.30')
   end
 
