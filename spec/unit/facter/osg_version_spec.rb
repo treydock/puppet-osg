a4e8f004d63f7413e3bda148d8bb5ff8bd546a2d
Trey Dockendorf <tdockendorf@osc.edu>
2020-03-05 11:18:51 -0500

    Fix unit tests

--- spec/unit/facter/osg_version_spec.rb
+++ spec/unit/facter/osg_version_spec.rb
@@ -11,13 +11,13 @@ describe 'osg_version fact' do
   end
 
   it 'returns correct version 3.2.30' do
-    allow(File).to receive(:exists?).with('/etc/osg-version').and_return(true)
+    allow(File).to receive(:exist?).with('/etc/osg-version').and_return(true)
     allow(Facter::Core::Execution).to receive(:execute).with('cat /etc/osg-version 2>/dev/null').and_return(my_fixture_read('osg-version-3.2.30'))
     expect(Facter.fact(:osg_version).value).to eq('3.2.30')
   end
 
   it 'returns nothing if /etc/osg-version is not present' do
-    allow(File).to receive(:exists?).with('/etc/osg-version').and_return(false)
+    allow(File).to receive(:exist?).with('/etc/osg-version').and_return(false)
     expect(Facter.fact(:osg_version).value).to be_nil
   end
 end
